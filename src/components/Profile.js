26466b7529c2791b30a417cdd27e4b51005aac16
Tahmid Jawad An-Noor <tjawad98@gmail.com>
2024-04-03 19:54:36 +0600

    profile image part implemented

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import { Buffer } from 'buffer';
+import axios from 'axios';
 
 function Profile() {
+    const token = localStorage.getItem('token');
+    const [base64Image, setBase64Image] = React.useState(null);
+    const { userId, firstName } = jwtDecode(token);
+
+    React.useEffect(() => {
+        axios.get(`http://localhost:3001/user/get-profile/${userId}`)
+            .then(response => response.data)
+            .then(data => {
+                const binaryImage = data.profileImage.data.data;
+                setBase64Image(Buffer.from(binaryImage, 'binary').toString('base64'));
+            })
+    }, [userId])
     return (
         <div className='profileContainer'>
             <div className='profileMenu'>
+                {base64Image && <div className='profileImage'>
+                    <img src={`data:image/jpg;base64,${base64Image}`} alt={firstName} />
+                    <i><h3>Hello, {firstName}</h3></i>
+                </div>}
                 <NavLink to='edit-profile'>Edit Profile</NavLink>
                 <NavLink to='upload-photo'>Upload Photo</NavLink>
                 <NavLink to='my-photos'>My Photos</NavLink>
